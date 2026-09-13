c30f34feca44cbe4b7700cbcd9483b8cb9e09385
Archi-bit <argennnmmmtv@gmail.com>
2026-09-13 20:24:19 +0600

    no no no no error plz piliz

--- my_site/settings.py
+++ my_site/settings.py
@@ -150,7 +150,7 @@ MODEL_TRANSLATION_LANGUAGES = ('en', 'ru')
 # https://docs.djangoproject.com/en/6.1/howto/static-files/
 
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = 'static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
